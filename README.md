d21ed90a2325455df9a02b336ee04693c12b7739
innerZachary <innerZacharyRaia@gmail.com>
2021-02-18 22:35:13 -0500

    Can draw on a variable amount of tiles

--- README.md
+++ README.md
@@ -12,5 +12,20 @@ on :hover I believe.
 Somehow i cannot even get basic CSS to work. I think I am either mentally tapped out or just crazy!
 
 2/18/21
-Deleted Everything. Hopefully that helps
+Deleted Everything. Hopefully that helps.
+
+It did!
+
+Okay, the major drawing functionality works correctly. I will call it quits for the night.
+Definitely a lot of cool features to add and learn about here!
+
+Still needs a button to reset.
+
+RGB Randomizer
+
+Eraser
+
+Opaque to Marie
+
+Generally improve appearance with Ayana's help.
 
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const container = document.querySelector('.container');
+
+// let startingTileAmount = prompt('How many tiles do you want?', 5);
+let startingTileAmount = 64;
+
+container.style.cssText = `grid-template-rows: repeat(${startingTileAmount}, 1fr); grid-template-columns: repeat(${startingTileAmount}, 1fr);`
+
+function divMaker(num) {
+  for(let i = 1; i <= (num * num); i++) {
+    let tile = document.createElement('div');
+    tile.classList = 'tile'; //className vs classList?
+    // tile.className = 'tile';
+    container.appendChild(tile)
+  }
+}
+
+divMaker(startingTileAmount);//placement of this matters, remember to ask matt?
+
+const tiles = document.querySelectorAll('.tile');
+
+tiles.forEach((tile) => {
+    tile.addEventListener('mouseover', () => {
+      tile.classList.add('moused');
+    })
+  });
